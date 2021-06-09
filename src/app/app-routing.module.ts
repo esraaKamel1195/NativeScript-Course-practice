9d190009b3d760ba6277986a6f4ceb5a5d85e8d4
Esraa Kamel <esraa.kamel1811@gmail.com>
2021-06-09 21:28:54 +0200

    Navigation between pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { Routes } from '@angular/router';
 import { NativeScriptRouterModule } from '@nativescript/angular';
 
 import { AuthComponent } from './auth/auth.component';
+import { TodayComponent } from './challenges/today/today.component';
 
 const routes: Routes = [
   { path: '', component: AuthComponent },
-
+  { path: 'today', component: TodayComponent }
 ]
 
 @NgModule({
